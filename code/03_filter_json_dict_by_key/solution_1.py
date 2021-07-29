f7a439cc4e7ebf4525d991f2e4e4c2e4c6d921c3
pyneng <pyneng.course@gmail.com>
2021-07-29 13:54:16 +0000

    Q&A 3. Filter JSON by key

--- code/03_filter_json_dict_by_key/solution_1.py
+++ code/03_filter_json_dict_by_key/solution_1.py
@@ -19,33 +19,21 @@ def filter_data_by_name(data, search_key):
     return results
 
 
-def filter_by_keys(data, keys):
-    if type(keys) == str:
-        keys = [keys]
-    for key in keys:
-        data = filter_data_by_name(data, key)
-    return data
-
-
 @click.command()
 @click.argument("json-file", type=click.File("r"))
-@click.argument("keys", nargs=-1, required=True)
-def cli(json_file, keys):
+@click.argument("key")
+def cli(json_file, key):
     """
     Filter data from JSON-FILE by key.
 
     Examples:
 
     \b
-        python solution_2_without_generator.py json_files/repos.json owner id
-        python solution_2_without_generator.py json_files/cfg.json name
-        python solution_2_without_generator.py json_files/cfg.json interface name
-        python solution_2_without_generator.py json_files/cfg.json address name
-        python solution_2_without_generator.py json_files/cfg.json login name
+        python solution_0.py json_files/basic.json name
 
     """
     data = json.load(json_file)
-    result = filter_by_keys(data, keys)
+    result = filter_data_by_name(data, key)
     print(result)
 
 
--- code/03_filter_json_dict_by_key/solution_2a.py
+++ code/03_filter_json_dict_by_key/solution_2a.py
@@ -19,21 +19,33 @@ def filter_data_by_name(data, search_key):
     return results
 
 
+def filter_by_keys(data, keys):
+    if type(keys) == str:
+        keys = [keys]
+    for key in keys:
+        data = filter_data_by_name(data, key)
+    return data
+
+
 @click.command()
 @click.argument("json-file", type=click.File("r"))
-@click.argument("key")
-def cli(json_file, key):
+@click.argument("keys", nargs=-1, required=True)
+def cli(json_file, keys):
     """
     Filter data from JSON-FILE by key.
 
     Examples:
 
     \b
-        python solution_0.py json_files/basic.json name
+        python solution_2_without_generator.py json_files/repos.json owner id
+        python solution_2_without_generator.py json_files/cfg.json name
+        python solution_2_without_generator.py json_files/cfg.json interface name
+        python solution_2_without_generator.py json_files/cfg.json address name
+        python solution_2_without_generator.py json_files/cfg.json login name
 
     """
     data = json.load(json_file)
-    result = filter_data_by_name(data, key)
+    result = filter_by_keys(data, keys)
     print(result)
 
 
